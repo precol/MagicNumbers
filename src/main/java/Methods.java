e1bcf4048d83dedf6b9300ee9d76f703dcc60fae
precol <mikolajpecold.lote@gmail.com>
2020-03-05 17:10:40 +0100

    no message

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Methods {
+
+    public ArrayList reader(){
+
+    }
+
+    public String byteGetter(){
+
+    }
+
+    public boolean compareBytes(){
+
+    }
+
+    public String getFileExtension(){
+
+    }
+
 }

084284ead245913ae3b1b7808828a0f2733542cb
precol <mikolajpecold.lote@gmail.com>
2020-03-05 17:26:24 +0100

    no message

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -5,11 +7,23 @@ import java.util.ArrayList;
 
 public class Methods {
 
-    public ArrayList reader(){
+    public ArrayList reader() throws IOException {
 
+        ArrayList<String[]> list = new ArrayList<>();
+        String line = "";
+        String[] toList;
+        BufferedReader bReader = new BufferedReader(new FileReader());
+
+        while((line = bReader.readLine()) != null){
+            toList =line.split(";");
+            list.add(toList);
+        }
+
+        return list;
     }
 
     public String byteGetter(String path) throws IOException {
+
         String filePath = path;
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
         StringBuilder builder =new StringBuilder();

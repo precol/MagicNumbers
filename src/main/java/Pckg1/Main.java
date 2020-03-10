cff89b28b2e4fe851fc952e2c56359c402caa59f
precol <mikolajpecold.lote@gmail.com>
2020-03-10 19:31:18 +0100

    no message

--- src/main/java/Pckg1/Main.java
+++ src/main/java/Pckg1/Main.java
@@ -1,3 +1,5 @@
+package Pckg1;
+
 import java.io.IOException;
 
 public class Main {
--- src/main/java/Pckg1/Methods.java
+++ src/main/java/Pckg1/Methods.java
@@ -1,3 +1,5 @@
+package Pckg1;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
